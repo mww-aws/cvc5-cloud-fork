9df93c359fa3afd7a57296a9ddd8a8accbb99678
Amalee Wilson <amalee@cs.stanford.edu>
2022-05-25 14:19:08 -0700

    wip

---
+++ local-mpi/mpi_cvc5.py
@@ -0,0 +1,24 @@
+from mpi4py import MPI
+import subprocess
+import pdb 
+
+partitioner = "/Users/amaleewilson/cvc5/build/bin/cvc5"
+strategy = "strict-cube"
+number_of_partitions = "4"
+output_file = "/Users/amaleewilson/aws-satcomp-solver-sample/local-mpi/partition_output"
+checks_before_partition = "500"
+checks_between_partitions = "500"
+partitioner_options = "--append-learned-literals-to-cubes"
+p0 = "--produce-learned-literals"
+smt_file = ("/Users/amaleewilson/Downloads/_array_monotonic.i_3_2_2.bpl_11.smt2")
+
+partition_command = [partitioner, 
+    f"--compute-partitions={number_of_partitions}",
+    f"--partition-strategy={strategy}",
+    f"--checks-before-partition={checks_before_partition}",
+    f"--write-partitions-to={output_file}",
+    f"--checks-between-partitions={checks_between_partitions}",
+    partitioner_options, p0,
+    smt_file]
+
+proc = subprocess.run(partition_command, check=True)
