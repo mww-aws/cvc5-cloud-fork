1d6f1ceec61af58a042d16268329138d859a0289
Amalee Wilson <amalee@cs.stanford.edu>
2022-06-20 10:18:27 -0700

    local mpi

--- local-mpi/mpi_cvc5.py
+++ local-mpi/mpi_cvc5.py
@@ -12,13 +12,26 @@ partitioner_options = "--append-learned-literals-to-cubes"
 p0 = "--produce-learned-literals"
 smt_file = ("/Users/amaleewilson/Downloads/_array_monotonic.i_3_2_2.bpl_11.smt2")
 
+# This version writes the partitions to a file. 
+# partition_command = [partitioner, 
+#     f"--compute-partitions={number_of_partitions}",
+#     f"--partition-strategy={strategy}",
+#     f"--checks-before-partition={checks_before_partition}",
+#     f"--write-partitions-to={output_file}",
+#     f"--checks-between-partitions={checks_between_partitions}",
+#     partitioner_options, p0,
+#     smt_file]
+# 
+# proc = subprocess.run(partition_command, check=True)
+
+# This version gets the output from the partition command. 
 partition_command = [partitioner, 
     f"--compute-partitions={number_of_partitions}",
     f"--partition-strategy={strategy}",
     f"--checks-before-partition={checks_before_partition}",
-    f"--write-partitions-to={output_file}",
     f"--checks-between-partitions={checks_between_partitions}",
     partitioner_options, p0,
     smt_file]
 
-proc = subprocess.run(partition_command, check=True)
+output = subprocess.check_output(partition_command).decode("utf-8").strip()
+print(output[0])
