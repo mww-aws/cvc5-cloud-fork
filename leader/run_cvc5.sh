22b27eee5ce59d2db528ef58c9389199fa3d6b88
Amalee Wilson <amalee@cs.stanford.edu>
2022-07-02 15:13:17 -0700

    fix error

--- leader/run_cvc5.sh
+++ leader/run_cvc5.sh
@@ -2,7 +2,10 @@
 
 echo $1
 echo $2
-mpirun  --allow-run-as-root -np 32 --hostfile $1 \
-  --report-bindings  \
+# mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np 2 \
+#   --hostfile $1 --use-hwthread-cpus --map-by node:PE=2 --bind-to none --report-bindings \
+# 
+mpirun --mca btl_tcp_if_include eth0  --allow-run-as-root -np $2 \
+  --hostfile $1  --use-hwthread-cpus --map-by node:PE=2 --bind-to none --report-bindings \
   python3.8 /competition/replace_solver.py /competition/cvc5 $3
 
