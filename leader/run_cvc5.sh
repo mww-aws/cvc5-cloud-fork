1da96629fd69efb6346e3d0054cc7c0932b7adac
Amalee Wilson <amalee@cs.stanford.edu>
2022-07-02 13:20:13 -0700

    wip

--- leader/run_cvc5.sh
+++ leader/run_cvc5.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-mpirun --allow-run-as-root -np $2 --hostfile $1 \
+echo $1
+echo $2
+mpirun  --allow-run-as-root -np 32 --hostfile $1 \
+  --report-bindings  \
   python3.8 /competition/replace_solver.py /competition/cvc5 $3
+
