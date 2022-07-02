9579c78d29e76848ac091021c93ccd35d112d6c3
Amalee Wilson <amalee@cs.stanford.edu>
2022-07-02 16:02:31 -0700

    hopefully using all the resources

--- leader/run_cvc5.sh
+++ leader/run_cvc5.sh
@@ -6,6 +6,6 @@ echo $2
 #   --hostfile $1 --use-hwthread-cpus --map-by node:PE=2 --bind-to none --report-bindings \
 # 
 mpirun --mca btl_tcp_if_include eth0  --allow-run-as-root -np $2 \
-  --hostfile $1  --use-hwthread-cpus --map-by node:PE=2 --bind-to none --report-bindings \
+  --hostfile $1  --use-hwthread-cpus --bind-to none --report-bindings \
   python3.8 /competition/replace_solver.py /competition/cvc5 $3
 
